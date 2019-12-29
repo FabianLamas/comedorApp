b70c59a15a3d5a53acbad1adeadd76c9eeeeda0c
flamas <flamas@tgv.com.ar>
2019-12-29 06:53:54 -0300

    first commit

---
+++ ReporteComedorUY/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace ReporteComedorUY
+{
+    partial class ReporteComedorUY
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fechaDesdePicker = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fechaHastaPicker = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fechaDesdePicker
+            // 
+            this.fechaDesdePicker.Location = new System.Drawing.Point(12, 44);
+            this.fechaDesdePicker.Name = "fechaDesdePicker";
+            this.fechaDesdePicker.Size = new System.Drawing.Size(274, 22);
+            this.fechaDesdePicker.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Desde: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(336, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hasta: ";
+            // 
+            // fechaHastaPicker
+            // 
+            this.fechaHastaPicker.Location = new System.Drawing.Point(339, 44);
+            this.fechaHastaPicker.Name = "fechaHastaPicker";
+            this.fechaHastaPicker.Size = new System.Drawing.Size(274, 22);
+            this.fechaHastaPicker.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(697, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 64);
+            this.button1.TabIndex = 2;
+            this.button1.Text = " Consultar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(811, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 64);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Descargar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 95);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(886, 435);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 533);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // ReporteComedorUY
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 576);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fechaHastaPicker);
+            this.Controls.Add(this.fechaDesdePicker);
+            this.Name = "ReporteComedorUY";
+            this.Text = "Reporte Consumos Uruguay";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker fechaDesdePicker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker fechaHastaPicker;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ReporteComedorUY/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReporteComedorUY
+{
+    public partial class ReporteComedorUY : Form
+    {
+        public ReporteComedorUY()
+        {
+            InitializeComponent();
+        }
+
+        private void GetConnection()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["defaultConnection"].ConnectionString;
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string _desde = fechaDesdePicker.Value.ToString("yyyyMMdd");
+            string _hasta = fechaHastaPicker.Value.ToString("yyyyMMdd");
+
+            label3.Text = _desde + " - " + _hasta;
+        }
+
+        
+    }
+}
