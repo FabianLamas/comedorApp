91252fb90d6b4d14cd47b7872714151d4c8abbac
Tedesco <tarviviana.tedesco@kof.com.mx>
2019-12-29 22:18:31 -0300

    fin del core

--- ReporteComedorUY/Form1.Designer.cs
+++ ReporteComedorUY/Form1.Designer.cs
@@ -35,47 +35,51 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // fechaDesdePicker
             // 
-            this.fechaDesdePicker.Location = new System.Drawing.Point(12, 44);
+            this.fechaDesdePicker.Location = new System.Drawing.Point(9, 36);
+            this.fechaDesdePicker.Margin = new System.Windows.Forms.Padding(2);
             this.fechaDesdePicker.Name = "fechaDesdePicker";
-            this.fechaDesdePicker.Size = new System.Drawing.Size(274, 22);
+            this.fechaDesdePicker.Size = new System.Drawing.Size(206, 20);
             this.fechaDesdePicker.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(9, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.Size = new System.Drawing.Size(44, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Desde: ";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(336, 9);
+            this.label2.Location = new System.Drawing.Point(252, 7);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 17);
+            this.label2.Size = new System.Drawing.Size(41, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Hasta: ";
             // 
             // fechaHastaPicker
             // 
-            this.fechaHastaPicker.Location = new System.Drawing.Point(339, 44);
+            this.fechaHastaPicker.Location = new System.Drawing.Point(254, 36);
+            this.fechaHastaPicker.Margin = new System.Windows.Forms.Padding(2);
             this.fechaHastaPicker.Name = "fechaHastaPicker";
-            this.fechaHastaPicker.Size = new System.Drawing.Size(274, 22);
+            this.fechaHastaPicker.Size = new System.Drawing.Size(206, 20);
             this.fechaHastaPicker.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(697, 13);
+            this.button1.Location = new System.Drawing.Point(523, 11);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(90, 64);
+            this.button1.Size = new System.Drawing.Size(68, 52);
             this.button1.TabIndex = 2;
             this.button1.Text = " Consultar";
             this.button1.UseVisualStyleBackColor = true;
@@ -83,38 +87,28 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(811, 13);
+            this.button2.Location = new System.Drawing.Point(608, 11);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(87, 64);
+            this.button2.Size = new System.Drawing.Size(65, 52);
             this.button2.TabIndex = 2;
             this.button2.Text = "Descargar";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 95);
+            this.dataGridView1.Location = new System.Drawing.Point(13, 87);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(886, 435);
-            this.dataGridView1.TabIndex = 3;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 533);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 17);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "label3";
+            this.dataGridView1.Size = new System.Drawing.Size(660, 369);
+            this.dataGridView1.TabIndex = 5;
             // 
             // ReporteComedorUY
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(922, 576);
-            this.Controls.Add(this.label3);
+            this.ClientSize = new System.Drawing.Size(692, 468);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -122,6 +116,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.fechaHastaPicker);
             this.Controls.Add(this.fechaDesdePicker);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "ReporteComedorUY";
             this.Text = "Reporte Consumos Uruguay";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -139,7 +134,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Label label3;
     }
 }
 
--- ReporteComedorUY/Form1.cs
+++ ReporteComedorUY/Form1.cs
@@ -3,26 +3,86 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OfficeExcel = Microsoft.Office.Interop.Excel;
 
 namespace ReporteComedorUY
 {
     public partial class ReporteComedorUY : Form
     {
+
+        private string desde = "";
+        private string hasta = "";
+
+
         public ReporteComedorUY()
         {
             InitializeComponent();
+            InitializeDate();
+            BuscarConsumos(desde, hasta);
         }
 
-        private void GetConnection()
+        private void InitializeDate()
         {
-            var connectionString = ConfigurationManager.ConnectionStrings["defaultConnection"].ConnectionString;
+            var currentMonth = DateTime.Now.Month;
+            var lastDay = DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month);
 
-            
+            fechaDesdePicker.Value = new DateTime(DateTime.Now.Year, currentMonth, 01);
+            fechaHastaPicker.Value = new DateTime(DateTime.Now.Year, currentMonth, lastDay);
+
+            desde = fechaDesdePicker.Value.ToString("yyyyMMdd");
+            hasta = fechaHastaPicker.Value.ToString("yyyyMMdd");
+        }
+
+        private void BuscarConsumos(string desde, string hasta)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["ApplicationDatabase"].ConnectionString;
+
+            using (SqlConnection sql = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("SP_CONSUMOS_TEST", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new SqlParameter("@Fecha_Desde", desde));
+                    cmd.Parameters.Add(new SqlParameter("@Fecha_Hasta", hasta));
+                    cmd.Parameters.Add(new SqlParameter("@CentroDeCosto", "Todos los centros"));
+
+                    DataSet ds = new DataSet();
+                    DataTable dt = new DataTable();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    sql.Open();
+                    da.Fill(dt);
+
+                    dataGridView1.DataSource = dt;
+                    dataGridView1.Show();
+                }
+            }
+
+            // BACKUP
+            //using (SqlConnection sql = new SqlConnection(connectionString))
+            //{
+            //    using (SqlCommand cmd = new SqlCommand("SP_CONSUMOS_TEST", sql))
+            //    {
+            //        cmd.CommandType = CommandType.StoredProcedure;
+            //        cmd.Parameters.Add(new SqlParameter("@Fecha_Desde", desde));
+            //        cmd.Parameters.Add(new SqlParameter("@Fecha_Hasta", hasta));
+            //        cmd.Parameters.Add(new SqlParameter("@CentroDeCosto", "Todos los centros"));
+
+            //        DataSet ds = new DataSet();
+            //        DataTable dt = new DataTable();
+            //        SqlDataAdapter da = new SqlDataAdapter(cmd);
+            //        sql.Open();
+            //        da.Fill(dt);
+
+            //        dataGridView1.DataSource = dt;
+            //        dataGridView1.Show();
+            //    }
+            //}
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -30,9 +90,46 @@ namespace ReporteComedorUY
             string _desde = fechaDesdePicker.Value.ToString("yyyyMMdd");
             string _hasta = fechaHastaPicker.Value.ToString("yyyyMMdd");
 
-            label3.Text = _desde + " - " + _hasta;
+            BuscarConsumos(_desde, _hasta);
         }
 
-        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OfficeExcel._Application app = new OfficeExcel.Application();
+            OfficeExcel._Workbook workbook = app.Workbooks.Add(Type.Missing);
+            OfficeExcel._Worksheet worksheet = null;
+
+            worksheet = workbook.Sheets["Hoja1"];
+            worksheet = workbook.ActiveSheet;
+            worksheet.Name = "CustomerDetails";
+
+            for (int i = 1; i < dataGridView1.Columns.Count+1; i++)
+            {
+                worksheet.Cells[1, i] = dataGridView1.Columns[i - 1].HeaderText;
+            }
+
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                {
+                    if(dataGridView1.Rows[i].Cells[j].Value != null)
+                    {
+                        worksheet.Cells[i + 2, j + 1] = dataGridView1.Rows[i].Cells[j].Value.ToString();
+                    }                 
+                }
+            }
+
+            var saveFileDialoge = new SaveFileDialog();
+            saveFileDialoge.FileName = "ReporteConsumosComedor UY";
+            saveFileDialoge.DefaultExt = ".xlsx";
+
+            if(saveFileDialoge.ShowDialog() == DialogResult.OK)
+            {
+                workbook.SaveAs(saveFileDialoge.FileName, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, OfficeExcel.XlSaveAsAccessMode.xlExclusive, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+
+            }
+
+            app.Quit();
+        }
     }
 }
